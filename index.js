dc6b11d5f1d9107dfb35d643a7cce6d959cd07ce
Freddy Barrios <freddybarrios.077@gmail.com>
2020-08-18 14:07:59 -0500

    Exploring mutations and inputs

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const app = require("express")();
+require("dotenv").config();
 const { graphqlHTTP } = require("express-graphql");
 const { makeExecutableSchema } = require("graphql-tools");
 const { readFileSync } = require("fs");
 const { join } = require("path");
 const resolvers = require("./lib/resolvers");
+const mongodbSvc = require("./lib/mongodbSvc");
 
 const PORT = process.env.PORT || 3000;
+
+mongodbSvc.connect();
 
 const typeDefs = readFileSync(
   join(__dirname, "lib", "schema.graphql"),
